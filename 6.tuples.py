8b7ccc115698fdae7558046db9c6e968055dd24e
Tanush Saha <tanush.saha05@gmail.com>
2025-05-04 13:59:12 +0530

    done

--- 6.tuples.py
+++ 6.tuples.py
@@ -5,4 +5,12 @@ print(type(tuple))
 
 
 # Write a code to check palindrome
+list1 = [1,3,3,1]
+copy_list1 = list1.copy()
+copy_list1.reverse()
+
+if(copy_list1 == list1):
+    print("Palindrome")
+else:
+    print("Not Palindrome")
 
