6f405edfd67ceb6116ff3a9c615b6ecd2da8ba1f
Tanush Saha <tanush.saha05@gmail.com>
2025-05-05 02:02:11 +0530

    done

---
+++ 7.Dictionaries.py
@@ -0,0 +1,32 @@
+dict = {
+    "key":"value",
+    "name":"Taush Saha",
+    "age":19,
+    "array":[12,5435,"Saha"]
+}
+# print(dict)
+
+dict['name'] = "Avi Saha"
+dict["title"] = True
+
+# print(dict)
+
+# Nested Dictionary
+dict1 = {
+    "name":"Tanush Saha",
+    "Subjects":{
+        "Math":90,
+        "Physics":77,
+        "Chemistry":89
+
+    }
+}
+
+# print(dict1["Subjects"]["Physics"])
+
+print(tuple(dict1.keys()))
+
+print()
+
+
+
